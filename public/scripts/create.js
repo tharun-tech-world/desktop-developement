36fdb428b3db1705da3a6e6a0b8973cced64647a
tharunpothuraj <tharunkumar1490@gmail.com>
2019-07-26 23:45:52 +0530

    .

--- public/scripts/create.js
+++ public/scripts/create.js
@@ -2,15 +2,14 @@ const fs= require('fs');
 const path= require('path');
 const remote = require('electron').remote;
 
-const crtbtn = document.getElementById('crtbtn');
-const fileName = document.getElementById('fileName');
-const fileContent = document.getElementById('fileContents');
+const crtbtn=document.getElementById('crtbtn');
+const fileName=document.getElementById('fileName');
+const fileContent=document.getElementById('fileContents');
 
-let pathName = path.join(__dirname, '../../Files');
-console.log(pathName);
+let pathName=path.join(__dirname, 'Files');
 crtbtn.addEventListener('click', function(){
-    let file = path.join(pathName, fileName.value)
-    let contents = fileContent.value;
+    let file=path.join(pathName, fileName)
+    let contents=fileContent.Value;
     
     fs.writeFile(file, contents, function(err){
         if(err){
@@ -19,8 +18,6 @@ crtbtn.addEventListener('click', function(){
         console.log("The file was created.")
 
     })
-    fileName.value = '';
-    fileContent.value = '';
 })
 
 
